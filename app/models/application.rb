4d3e0f95dbbe299993d511d32bc6a30324faf7fe
Bryce Wein <bryce.wein@gmail.com>
2022-05-19 15:05:20 -0400

    Test/Feat: validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :reason, presence: true
+  validates :zip, presence: true, numericality: true
+
   has_many :application_pets
   has_many :pets, through: :application_pets
 end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Application, type: :model do
     it { should have_many(:application_pets) }
     it { should have_many(:pets).through(:application_pets) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:reason) }
+    it { should validate_numericality_of(:zip) }
+  end
 end
