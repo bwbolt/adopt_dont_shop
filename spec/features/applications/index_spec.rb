6a9dd1cc5d4443849b07cc1a2d4dba251bccc1d5
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-19 16:33:23 -0600

    refactor test description

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Applications#index", type: :feature do
   describe "index" do
-    it "can create a new application" do
+    it "displays all submitted applications" do
       shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet1 = shelter.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
       application = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
