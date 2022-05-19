13e770232c64541381e46b6391d53bf8d3249c82
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-19 16:08:00 -0600

    feat: applications#new, applications#index

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationsController < ApplicationController
+  def index
+    @applications = Application.all
+  end
+
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    applicant = Application.create(applicant_params)
+    redirect_to "/applications"
+  end
+
+  private
+    def applicant_params
+      params.permit(:name, :address, :city, :state, :zip, :reason)
+    end
 end
--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Applications#index", type: :feature do
   describe "index" do
-    shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet1 = shelter.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
-    application = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
-                                     zip: 12_345, reason: 'I like dogs')
-    ApplicationPet.create!(application_id: application.id, pet_id: pet1.id)
+    it "can create a new application" do
+      shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet1 = shelter.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
+      application = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                       zip: 12_345, reason: 'I like dogs')
+      ApplicationPet.create!(application_id: application.id, pet_id: pet1.id)
 
-    visit "/applications"
+      visit "/applications"
 
-    expect(page).to have_content("Zach Hazelwood")
+      expect(page).to have_content("Zach Hazelwood")
+      expect(page).to have_link("New Application")
+    end
   end
 end
