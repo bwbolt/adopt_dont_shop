4c99a92e7270def9b8c82ccb86d46c2fc12faa38
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-19 16:39:21 -0600

    test: Application submission errors

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -31,4 +31,47 @@ RSpec.describe 'applications#new', type: :feature do
       expect(page).to have_content("John Smith")
     end
   end
+
+  describe "Missing Information Submission Errors" do
+    it "displays a message when an Application form field is left empty" do
+      # Missing Name
+      visit "/applications/new"
+
+      # fill_in :name, with: "John Smith"
+      fill_in :address, with: "222 Main St"
+      fill_in :city, with: "Littleton"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: 80808
+      fill_in :reason, with: "I just think they're neat."
+
+      click_button("Submit Application")
+      expect(page).to have_content("Please enter your Name")
+
+      # Missing Address
+      visit "/applications/new"
+
+      fill_in :name, with: "John Smith"
+      # fill_in :address, with: "222 Main St"
+      fill_in :city, with: "Littleton"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: 80808
+      fill_in :reason, with: "I just think they're neat."
+
+      click_button("Submit Application")
+      expect(page).to have_content("Please enter your Address")
+
+      # Missing Reason for Adoption
+      visit "/applications/new"
+
+      fill_in :name, with: "John Smith"
+      fill_in :address, with: "222 Main St"
+      fill_in :city, with: "Littleton"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: 80808
+      # fill_in :reason, with: "I just think they're neat."
+
+      click_button("Submit Application")
+      expect(page).to have_content("Why would you like to adopt this pet?")
+    end
+  end
 end
