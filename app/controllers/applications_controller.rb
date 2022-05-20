b40634b1469bdeb16c3e6fa58ab1d6d63bf6c513
Bryce Wein <bryce.wein@gmail.com>
2022-05-19 20:41:55 -0400

    Feat: add form to search for pet/ add to show method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,39 +5,44 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+    @adoptable_pets = if !params[:pets_by_name].nil? && params[:pets_by_name] != ''
+                        Pet.where(name: params[:pets_by_name])
+                      else
+                        []
+                      end
   end
 
-  def new
-  end
+  def new; end
 
   def create
     applicant = Application.new(applicant_params)
     if applicant.save
-      flash[:success] = "Your application has been received."
-      redirect_to "/applications"
+      flash[:success] = 'Your application has been received.'
+      redirect_to '/applications'
     elsif applicant.errors.full_messages == ["Name can't be blank"]
-      flash[:notice] = "Please enter your Name"
+      flash[:notice] = 'Please enter your Name'
       render :new
     elsif applicant.errors.full_messages == ["Address can't be blank"]
-      flash[:notice] = "Please enter your Address"
+      flash[:notice] = 'Please enter your Address'
       render :new
     elsif applicant.errors.full_messages == ["City can't be blank"]
-      flash[:notice] = "Please enter your City"
+      flash[:notice] = 'Please enter your City'
       render :new
     elsif applicant.errors.full_messages == ["State can't be blank"]
-      flash[:notice] = "Please enter your State"
+      flash[:notice] = 'Please enter your State'
       render :new
     elsif applicant.errors.full_messages == ["Zip can't be blank"]
-      flash[:notice] = "Please enter your Zip Code"
+      flash[:notice] = 'Please enter your Zip Code'
       render :new
     elsif applicant.errors.full_messages == ["Reason can't be blank"]
-      flash[:notice] = "Why would you like to adopt this pet?"
+      flash[:notice] = 'Why would you like to adopt this pet?'
       render :new
     end
   end
 
   private
-    def applicant_params
-      params.permit(:name, :address, :city, :state, :zip, :reason)
-    end
+
+  def applicant_params
+    params.permit(:name, :address, :city, :state, :zip, :reason)
+  end
 end
