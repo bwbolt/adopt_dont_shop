bc6bb1380d9ebb5ba777af5b2b8efc64652a7393
Bryce Wein <bryce.wein@gmail.com>
2022-05-19 21:38:53 -0400

    Feat: adopt pet button and applicationpets creation method

--- config/routes.rb
+++ config/routes.rb
@@ -42,4 +42,5 @@ Rails.application.routes.draw do
   post '/applications', to: 'applications#create'
   get '/applications/:id', to: 'applications#show'
 
+  post '/application_pets', to: 'application_pets#create'
 end
