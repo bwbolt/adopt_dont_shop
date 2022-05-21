c5a848fed941e4148d8db2f7c535215664d0b4be
Bryce Wein <bryce.wein@gmail.com>
2022-05-21 19:41:35 -0400

    Feat: approve/reject pet adoption

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,4 +1,8 @@
 class ApplicationPet < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def self.search(pet_id, application_id)
+    where(pet_id: pet_id).where(application_id: application_id).first
+  end
 end
