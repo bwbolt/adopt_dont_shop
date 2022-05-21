30d363fc4abc727fb4891b333439a42423a47475
Bryce Wein <bryce.wein@gmail.com>
2022-05-21 19:39:01 -0400

    Test: add tests of approving/rejecting

---
+++ spec/features/admin/applications/show_spec.rb
@@ -0,0 +1,97 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Application Show' do
+  describe 'Approving/Rejecting Pet for adoption' do
+    it 'has a working approve adoption button' do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      zach = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                zip: 12_345, reason: 'I like dogs')
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_2.id)
+      zach.update(status: 'Pending')
+      visit "/admin/applications/#{zach.id}"
+      within "#pet-#{pet_1.id}" do
+        expect(page).to have_link(pet_1.name)
+        expect(page).to have_button('Approve')
+      end
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to have_button('Approve')
+        click_on 'Approve'
+      end
+      expect(current_path).to eq("/admin/applications/#{zach.id}")
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to_not have_button('Approve')
+        expect(page).to have_content('Approved!!')
+      end
+    end
+
+    it 'has a working reject adoption button' do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      zach = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                zip: 12_345, reason: 'I like dogs')
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_2.id)
+      zach.update(status: 'Pending')
+      visit "/admin/applications/#{zach.id}"
+      within "#pet-#{pet_1.id}" do
+        expect(page).to have_link(pet_1.name)
+        expect(page).to have_button('Reject')
+      end
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to have_button('Reject')
+        click_on 'Reject'
+      end
+      expect(current_path).to eq("/admin/applications/#{zach.id}")
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to_not have_button('Reject')
+        expect(page).to have_content('Rejected!!')
+      end
+    end
+
+    it 'Approved/Rejected Pets on one Application do not affect other Applications' do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      zach = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                zip: 12_345, reason: 'I like dogs')
+      bryce = Application.create(name: 'Bryce Hazelwood', address: '1234 Fake Lane', city: 'Realtown', state: 'VA',
+                                 zip: 23_322, reason: 'I like to eat dogs')
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_2.id)
+      ApplicationPet.create!(application_id: bryce.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: bryce.id, pet_id: pet_2.id)
+      zach.update(status: 'Pending')
+      bryce.update(status: 'Pending')
+      visit "/admin/applications/#{zach.id}"
+      within "#pet-#{pet_1.id}" do
+        expect(page).to have_link(pet_1.name)
+        expect(page).to have_button('Reject')
+      end
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to have_button('Reject')
+        click_on 'Reject'
+      end
+      expect(current_path).to eq("/admin/applications/#{zach.id}")
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to_not have_button('Reject')
+        expect(page).to have_content('Rejected!!')
+      end
+      visit "/admin/applications/#{bryce.id}"
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to have_button('Reject')
+        expect(page).to have_button('Approve')
+      end
+    end
+  end
+end
