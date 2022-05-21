62a86bf489e16c0b1d8645f70e784f58f303ffbf
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-21 10:39:24 -0600

    test: Shelters with pending applications

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Admin Shelter Index" do
     # As a visitor
     # When I visit the admin shelter index ('/admin/shelters')
     # Then I see all Shelters in the system listed in reverse alphabetical order by name
+
     it "displays shelter information to an admin in reverse alphabetical order" do
       foothills = Shelter.create(name: 'Foothills Animal Shelter', city: 'Golden, CO', foster_program: true, rank: 3)
       aurora = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
@@ -14,6 +15,28 @@ RSpec.describe "Admin Shelter Index" do
 
       expect(page).to have_content(aurora.name)
       expect(dfl.name).to appear_before(aurora.name)
+    end
+  end
+
+  describe "User Story 16" do
+    # As a visitor
+    # When I visit the admin shelter index ('/admin/shelters')
+    # Then I see a section for "Shelter's with Pending Applications"
+    # And in this section I see the name of every shelter that has a pending application
+
+    it "displays shelters with pending applications" do
+      foothills = Shelter.create(name: 'Foothills Animal Shelter', city: 'Golden, CO', foster_program: true, rank: 3)
+      aurora = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      dfl = Shelter.create(name: 'Dumb Friends League', city: 'Denver, CO', foster_program: true, rank: 2)
+
+      pet1 = aurora.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
+      application = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                       zip: 12_345, reason: 'I like dogs')
+      ApplicationPet.create!(application_id: application.id, pet_id: pet1.id)
+
+      visit '/admin/shelters'
+
+      expect("Shelters With Pending Applications").to appear_before(aurora.name)
     end
   end
 end
