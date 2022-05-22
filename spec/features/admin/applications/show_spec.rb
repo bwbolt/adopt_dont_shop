902c75e7b6d1bbdebddb67d94f2328bf12ba531b
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-22 14:30:08 -0600

    test: Pet approval only Once

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -153,5 +153,42 @@ RSpec.describe 'Admin Application Show' do
       visit "/pets/#{pet_1.id}"
       expect(page).to have_content('false')
     end
+
+    it 'if approved on an approved application, no longer are up for approval on another application' do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      zach = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                zip: 12_345, reason: 'I like dogs')
+      bryce = Application.create(name: 'Bryce Hazelwood', address: '1234 Fake Lane', city: 'Realtown', state: 'VA',
+                                 zip: 23_322, reason: 'I like to eat dogs')
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_2.id)
+      ApplicationPet.create!(application_id: bryce.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: bryce.id, pet_id: pet_2.id)
+      zach.update(status: 'Pending')
+      bryce.update(status: 'Pending')
+      visit "/admin/applications/#{zach.id}"
+      within "#pet-#{pet_1.id}" do
+        click_on 'Approve'
+      end
+      within "#pet-#{pet_2.id}" do
+        click_on 'Approve'
+      end
+      visit "/admin/applications/#{bryce.id}"
+
+      within "#pet-#{pet_1.id}" do
+        expect(page).to have_link(pet_1.name)
+        expect(page).to have_button('Reject')
+        expect(page).to_not have_button('Approve')
+        expect(page).to have_content('This pet has been approved for adoption, sorry!')
+      end
+      within "#pet-#{pet_2.id}" do
+        expect(page).to have_link(pet_2.name)
+        expect(page).to have_button('Reject')
+        expect(page).to_not have_button('Approve')
+        expect(page).to have_content('This pet has been approved for adoption, sorry!')
+      end
+    end
   end
 end
