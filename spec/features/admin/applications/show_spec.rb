9822baf612c98301e1d8a321887ff87f8fb7f4bc
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-22 14:22:05 -0600

    test: Update pet adoption status

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -133,5 +133,25 @@ RSpec.describe 'Admin Application Show' do
       end
       expect(page).to have_content('Application Status: Rejected')
     end
+
+    it 'Application Approval makes Pets not adoptable' do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      zach = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                zip: 12_345, reason: 'I like dogs')
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: zach.id, pet_id: pet_2.id)
+      zach.update(status: 'Pending')
+      visit "/admin/applications/#{zach.id}"
+      within "#pet-#{pet_1.id}" do
+        click_on 'Approve'
+      end
+      within "#pet-#{pet_2.id}" do
+        click_on 'Approve'
+      end
+      visit "/pets/#{pet_1.id}"
+      expect(page).to have_content('false')
+    end
   end
 end
