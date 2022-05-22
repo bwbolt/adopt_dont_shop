955ef42be6f8e04f4809f35aa331fe478a707afd
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-22 14:49:34 -0600

    feat: admin_applications#index

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ Rails.application.routes.draw do
 
   get '/admin/shelters', to: 'admin_shelters#index'
 
+  get '/admin/applications', to: 'admin_applications#index'
   get '/admin/applications/:id', to: 'admin_applications#show'
 end
 
