2a5bf1b9bf1758261bcf4805d3ec0d421e48dec6
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-23 12:03:02 -0600

    test: list pets on pending application

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -73,4 +73,46 @@ RSpec.describe "admin_shelters#show" do
       end
     end
   end
+
+  describe "User Story 2" do
+    # As a visitor
+    # When I visit an admin shelter show page
+    # Then I see a section titled "Action Required"
+    # In that section, I see a list of all pets for this shelter that have a pending application and have not yet been marked "approved" or "rejected"
+    it "has an 'Action Required' section for pets on a 'Pending' application" do
+      shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = shelter.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      pet_3 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+      pet_4 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Big Dog', shelter_id: shelter.id)
+      application = Application.create(name: 'Zach Hazelwood', address: '1234 Fake Street', city: 'Faketown', state: 'CO',
+                                       zip: 12_345, reason: 'I like dogs')
+      ApplicationPet.create!(application_id: application.id, pet_id: pet_1.id)
+      ApplicationPet.create!(application_id: application.id, pet_id: pet_2.id)
+      ApplicationPet.create!(application_id: application.id, pet_id: pet_3.id)
+
+      application.update(status: "Pending")
+
+      visit "/admin/shelters/#{shelter.id}"
+
+      within('#action_required') do
+        expect(page).to have_content("Action Required")
+        expect(page).to have_content(pet_1.name)
+        expect(page).to have_content(pet_2.name)
+        expect(page).to have_content(pet_3.name)
+        expect(page).to_not have_content(pet_4.name)
+      end
+
+      application.update(status: "Approved")
+
+      visit "/admin/shelters/#{shelter.id}"
+
+      within('#action_required') do
+        expect(page).to have_content("Action Required")
+        expect(page).to_not have_content(pet_1.name)
+        expect(page).to_not have_content(pet_2.name)
+        expect(page).to_not have_content(pet_3.name)
+      end
+    end
+  end
 end
