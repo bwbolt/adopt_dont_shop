0784e3204e5260c936d1a53c33acaef32078da72
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-23 10:59:12 -0600

    test: average_age on #show

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe "admin_shelters#show" do
       expect(page).to have_content(shelter.city)
     end
   end
+
+  describe "User Story 5" do
+    # As a visitor
+    # When I visit an admin shelter show page
+    # Then I see a section for statistics
+    # And in that section I see the average age of all adoptable pets for that shelter
+    it "has an average Pet:age statistic" do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      pet_3 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+      visit "admin/shelters/#{shelter.id}"
+
+      within('#statistics') do
+        expect(page).to have_content("Statistics:")
+        expect(page).to have_content("Average Age of Adoptable Pets: 2")
+      end
+    end
+  end
 end
