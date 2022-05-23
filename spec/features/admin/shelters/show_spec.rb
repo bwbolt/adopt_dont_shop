30761d0efe26c9a83bccc9bd77c8a66816c5440e
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-23 11:03:58 -0600

    test: count adoptable pets

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -35,4 +35,23 @@ RSpec.describe "admin_shelters#show" do
       end
     end
   end
+
+  describe "User Story 4" do
+    # As a visitor
+    # When I visit an admin shelter show page
+    # Then I see a section for statistics
+    # And in that section I see the number of pets at that shelter that are adoptable
+    it "has a count of adoptable pets" do
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+      pet_3 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+      visit "admin/shelters/#{shelter.id}"
+
+      within('#statistics') do
+        expect(page).to have_content("Statistics:")
+        expect(page).to have_content("Number of Adoptable Pets: 3")
+      end
+    end
+  end
 end
