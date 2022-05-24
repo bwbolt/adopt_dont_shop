d7faccc941284703f3197e1a3711fb0b78da77d6
Bryce Wein <bryce.wein@gmail.com>
2022-05-23 20:38:46 -0400

    Refactor: add appropriate spacing

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -1,13 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "admin_shelters#show" do
-  describe "admin_shelters#show find_by_sql" do
-    # Admin Shelters Show Page
-    #
-    # As a visitor
-    # When I visit an admin shelter show page
-    # Then I see that shelter's name and full address
-    it "shows Shelter info to admin" do
+RSpec.describe 'admin_shelters#show' do
+  describe 'admin_shelters#show find_by_sql' do
+    it 'shows Shelter info to admin' do
       shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 
       visit "admin/shelters/#{shelter.id}"
@@ -17,68 +12,55 @@ RSpec.describe "admin_shelters#show" do
     end
   end
 
-  describe "User Story 5" do
-    # As a visitor
-    # When I visit an admin shelter show page
-    # Then I see a section for statistics
-    # And in that section I see the average age of all adoptable pets for that shelter
-    it "has an average Pet:age statistic" do
+  describe 'User Story 5' do
+    it 'has an average Pet:age statistic' do
       shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
       pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
       pet_3 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+
       visit "admin/shelters/#{shelter.id}"
 
       within('#statistics') do
-        expect(page).to have_content("Statistics:")
-        expect(page).to have_content("Average Age of Adoptable Pets: 2")
+        expect(page).to have_content('Statistics:')
+        expect(page).to have_content('Average Age of Adoptable Pets: 2')
       end
     end
   end
 
-  describe "User Story 4" do
-    # As a visitor
-    # When I visit an admin shelter show page
-    # Then I see a section for statistics
-    # And in that section I see the number of pets at that shelter that are adoptable
-    it "has a count of adoptable pets" do
+  describe 'User Story 4' do
+    it 'has a count of adoptable pets' do
       shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
       pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
       pet_3 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+
       visit "admin/shelters/#{shelter.id}"
 
       within('#statistics') do
-        expect(page).to have_content("Statistics:")
-        expect(page).to have_content("Number of Adoptable Pets: 3")
+        expect(page).to have_content('Statistics:')
+        expect(page).to have_content('Number of Adoptable Pets: 3')
       end
     end
   end
 
-  describe "User Story 3" do
-    # As a visitor
-    # When I visit an admin shelter show page
-    # Then I see a section for statistics
-    # And in that section I see the number of pets that have been adopted from that shelter
-    it "has a count of adopted pets" do
+  describe 'User Story 3' do
+    it 'has a count of adopted pets' do
       shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = Pet.create(adoptable: false, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
       pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
       pet_3 = Pet.create(adoptable: true, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
+
       visit "admin/shelters/#{shelter.id}"
 
       within('#statistics') do
-        expect(page).to have_content("Statistics:")
-        expect(page).to have_content("Number of Adopted Pets: 1")
+        expect(page).to have_content('Statistics:')
+        expect(page).to have_content('Number of Adopted Pets: 1')
       end
     end
   end
 
-  describe "User Story 2" do
-    # As a visitor
-    # When I visit an admin shelter show page
-    # Then I see a section titled "Action Required"
-    # In that section, I see a list of all pets for this shelter that have a pending application and have not yet been marked "approved" or "rejected"
+  describe 'User Story 2' do
     it "has an 'Action Required' section for pets on a 'Pending' application" do
       shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = shelter.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
@@ -91,24 +73,24 @@ RSpec.describe "admin_shelters#show" do
       ApplicationPet.create!(application_id: application.id, pet_id: pet_2.id)
       ApplicationPet.create!(application_id: application.id, pet_id: pet_3.id)
 
-      application.update(status: "Pending")
+      application.update(status: 'Pending')
 
       visit "/admin/shelters/#{shelter.id}"
 
       within('#action_required') do
-        expect(page).to have_content("Action Required")
+        expect(page).to have_content('Action Required')
         expect(page).to have_content(pet_1.name)
         expect(page).to have_content(pet_2.name)
         expect(page).to have_content(pet_3.name)
         expect(page).to_not have_content(pet_4.name)
       end
 
-      application.update(status: "Approved")
+      application.update(status: 'Approved')
 
       visit "/admin/shelters/#{shelter.id}"
 
       within('#action_required') do
-        expect(page).to have_content("Action Required")
+        expect(page).to have_content('Action Required')
         expect(page).to_not have_content(pet_1.name)
         expect(page).to_not have_content(pet_2.name)
         expect(page).to_not have_content(pet_3.name)
@@ -116,11 +98,7 @@ RSpec.describe "admin_shelters#show" do
     end
   end
 
-  describe "User Story 1" do
-    # As a visitor
-    # When I visit an admin shelter show page
-    # And I look in the "Action Required" section
-    # Then next to each pet's name I see a link to the admin application show page where I can accept or reject the pet.
+  describe 'User Story 1' do
     it "has an 'Action Required' section with link to application#show for each" do
       shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = shelter.pets.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald')
@@ -133,12 +111,12 @@ RSpec.describe "admin_shelters#show" do
       ApplicationPet.create!(application_id: application.id, pet_id: pet_2.id)
       ApplicationPet.create!(application_id: application.id, pet_id: pet_3.id)
 
-      application.update(status: "Pending")
+      application.update(status: 'Pending')
 
       visit "/admin/shelters/#{shelter.id}"
 
       within('#action_required') do
-        expect(page).to have_content("Action Required")
+        expect(page).to have_content('Action Required')
         expect(page).to have_content(pet_1.name)
         expect(page).to have_link("#{pet_1.name} Application")
         expect(page).to have_content(pet_2.name)
@@ -147,16 +125,17 @@ RSpec.describe "admin_shelters#show" do
         expect(page).to have_link("#{pet_3.name} Application")
         expect(page).to_not have_content(pet_4.name)
 
-        click_link ("#{pet_1.name} Application")
+        click_link("#{pet_1.name} Application")
+
         expect(current_path).to eq("/admin/applications/#{application.id}")
       end
 
-      application.update(status: "Approved")
+      application.update(status: 'Approved')
 
       visit "/admin/shelters/#{shelter.id}"
 
       within('#action_required') do
-        expect(page).to have_content("Action Required")
+        expect(page).to have_content('Action Required')
         expect(page).to_not have_content(pet_1.name)
         expect(page).to_not have_link("#{pet_1.name} Application")
       end
